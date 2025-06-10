cdc247212673e8162386c132112d4759f0373def
BENJAMINDTS <122805360+BENJAMINDTS@users.noreply.github.com>
2025-06-10 17:32:03 +0200

    Delete First_PHP_Codes/Pegasus directory

--- First_PHP_Codes/Pegasus/views/hospitales/show.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/../../config/config.php';
-?>
-<h1>Detalles del Hospital</h1>
-<p><strong>ID:</strong> <?= htmlspecialchars($hospital['id']) ?></p>
-<p><strong>Nombre:</strong> <?= htmlspecialchars($hospital['nombre']) ?></p>
-<a href="<?= BASE_URL ?>hospitales">Volver</a>
